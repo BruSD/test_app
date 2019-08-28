93e6173fea131c1d5a545e3189973bb0585b169b
Brusd <brusd.com@gmail.com>
2019-08-28 14:09:13 +0300

    -run random color generator onClick to AppBar,

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -49,6 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   Color _randomColor;
   Random _random = Random();
+  double _sBarH = 55;
+  String _sBarTitle = " ";
 
   void _incrementCounter() {
     getRandomColor();
@@ -90,46 +93,62 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: _randomColor,
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          floating: true,
+          pinned: true,
+          expandedHeight: _sBarH,
+
+          backgroundColor: Colors.orangeAccent,
+          flexibleSpace: Container(
+              child: InkWell(
+            onTap: () {
+              getRandomColor();
+              setState(() {
+                if (_sBarH == 55) {
+                  _sBarH = 200;
+                  _sBarTitle =
+                      "Is this what you expect or it must be extended Appbar with own BlackJack and other stuff?:)";
+                } else {
+                  _sBarH = 55;
+                  _sBarTitle = " ";
+                }
+              });
+            },
+            child: Center(
+              child: Text(
+                _sBarTitle,
+                style: TextStyle(fontSize: 22.0, color: Colors.white),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          )),
         ),
-      ),
+        SliverFillRemaining(
+          child: InkWell(
+              onTap: () {
+                getRandomColor();
+              },
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              )),
+        ),
+      ]),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
